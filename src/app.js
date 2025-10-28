410af3480967551e1998668365537376a91734cf
Brayam Lopez <brayam0055@gmail.com>
2025-10-28 12:22:19 -0600

    more payload for POST

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ conectarDB();
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Rutas
 app.use('/api/proyectos', proyectoRoutes);
